ea9568c4062c794d81d2ab9b503ac367459b84c2
Jelle Slowack <jslowack@inimco.com>
2024-10-29 21:59:35 +0100

    chore: add electron-forge

---
+++ forge.config.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  packagerConfig: {
+    ignore: [
+      /^\/src/,
+      /(.eslintrc.json)|(.gitignore)|(electron.vite.config.ts)|(forge.config.cjs)|(tsconfig.*)/
+    ]
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {}
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin']
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {}
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {}
+    }
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'jslowack',
+          name: 'azure-messaging-explorer'
+        },
+        prerelease: true
+      }
+    }
+  ]
+}
